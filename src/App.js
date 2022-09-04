005e4457a1d2e283dbec0f7b1b943adc78bb410a
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-09-05 06:47:10 +0800

    init tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import NavBar from "./NavBar"
+import {useState, useEffect} from "react";
 function App() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  useEffect(() => {
+    console.log(screenSize)
+    window.addEventListener("resize", () => {
+      setScreenSize(window.innerWidth);
+    });
+  }, [screenSize]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar screenSize = {screenSize}/>
     </div>
   );
 }
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,18 +1,28 @@
 import {useState, useEffect} from 'react';
 
-const NavBar = () => {
+const NavBar = (props) => {
     const [collapse, setCollapse] = useState(false);
-
+    const [openNav, setOpenNav] = useState(false);
     useEffect(() => {
+        if (props.screenSize < 500){
+            console.log("?")
+            setCollapse(true);
+            setOpenNav(false);
+        }
         if (collapse) {
             document.body.classList.add('nav-open');
         } else {
             document.body.classList.remove('nav-open');
         }
-    }, [collapse]);
+    }, [collapse, props]);
     return (
-        <nav className = "w-screen bg-white fixed top-0 z-50 shadow-md" >
-            
+        <nav className = "w-screen h-auto bg-black fixed top-0 z-50 shadow-md p-2" >
+            <div className = "m-2">
+                <p className = "p-2 text-white">
+                    asd
+                </p>
+            </div>
         </nav>
     )
 }
+export default NavBar;
